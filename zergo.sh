cce4b64c05bb4a40fb20263fdc3ea8abf719058e
thepowert <96099226+thepowert@users.noreply.github.com>
2022-01-03 15:23:39 +0700

    Update zergo.sh

--- zergo.sh
+++ zergo.sh
@@ -1,2 +1,2 @@
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
-./cpuminer -a yespower -o stratum+tcp://yespower.mine.zergpool.com:6533 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7 -p c=CPMK,ID=$(echo $(shuf -i 1-2000 -n 1)-DUwER)
+./cpuminer -a yespower -o stratum+tcp://yespower.mine.zergpool.com:6533 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7 -p c=CPMK,ID=$(echo $(shuf -i 1-9000 -n 1)-DUwER)
